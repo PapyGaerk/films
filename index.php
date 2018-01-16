506230196f6b198675d6d12b1216935950e6ebc5
Simon Gioffred <Simon.Gioffredi@gmail.com>
2018-01-16 23:23:07 +0100

    DÃ©butdu developpement front, creation de la page connexion / inscripton

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+	<head>
+		<title>Films critique</title>
+		<meta charset="UTF-8"/>
+		<meta name="author" content="Jean Deck, Simon Gioffredi"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+		<meta name="description" content="Bienvenue sur le site de critique de films. Enjoy !"/>
+		<link href="assets/css/reset.css" rel="stylesheet">
+		<link href="assets/css/style.css" rel="stylesheet">
+
+		<script src="assets/js/minified/jquery.min.js"></script>
+		<script src="assets/js/minified/TweenMax.min.js"></script>
+
+		<script src="assets/js/minified/lottie.js"></script>
+		<script src="assets/js/main.js"></script>
+	</head>
+	<body>
+		<header>
+			<nav>
+				<ul>
+					<li>
+						<a href="#">Logo</a>
+					</li>
+				</ul>
+				<ul class="center">
+					<li>
+						<span class="loupe">
+							<label for="research"></label>
+							<input id="research" name="research" type="text"
+								   placeholder="Films, Acteurs...">
+						</span>
+					</li>
+					<li>
+						<a href="#">Home</a>
+					</li>
+					<li>
+						<a href="#">Explore</a>
+					</li>
+					<li>
+						<a href="#">A voir</a>
+					</li>
+				</ul>
+				<ul>
+					<li>
+						<a href="#">Se connecter</a>
+					</li>
+				</ul>
+			</nav>
+		</header>
+		<section class="container">
+			<article class="monde">
+				<div>
+					<img src="assets/img/map.png" alt="La map monde">
+					<p class="connexion_desc">Suivez les utilisateurs actifs n'importe ou dans le monde pour garder un
+						oeil sur leurs derniers
+						films vus, meilleurs films et notes</p>
+				</div>
+			</article>
+			<article class="connexion_card">
+				<div>
+					<h1>Se Connecter</h1>
+				</div>
+			</article>
+		</section>
+	</body>
+</html>
